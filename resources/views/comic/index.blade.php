efb7b82b1cdf9fb47fa7e462e5a78f2f119ea173
albertoferri <falberto99@gmail.com>
2024-04-11 17:15:00 +0200

    stampati fumetti sotto forma di card

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -3,16 +3,31 @@
 @section('content')
     <div class="container py-5">
 
-        <h1>COMICS</h1>
+        <h1 class="text-center display-1 fw-bold mb-5">COMICS</h1>
 
 
-        @foreach ($comics as $comic)
-
-        <img src="{{$comic->thumb}}" alt="">
-        {{$comic->title}}
-        {{$comic->description}}
-            
-        @endforeach
+        <div class="row row-gap-4">
+            @foreach($comics as $comic)
+                <div class="col-sm-4">
+                    <div class="card h-100">
+                        <img class="card-img-top" src="{{$comic->thumb}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$comic->title}}</h5>
+                            <p class="card-text">{{$comic->description}}</p>
+                            <ul>
+                                <li>Price: {{$comic->price}}</li>
+                                <li>Series: {{$comic->series}}</li>
+                                <li>Sale Date: {{$comic->sale_date}}</li>
+                                <li>Type: {{$comic->type}}</li>
+                                <li>Artists: {{$comic->artists}}</li>
+                                <li>Writers: {{$comic->writers}}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        
         
 
     </div>
